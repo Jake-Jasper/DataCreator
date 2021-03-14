4ab7bfda3f4fcad5d2c54d44235937b373091224
jake <jakeljasper@gmail.com>
2021-03-14 14:04:23 +0000

    refactored - created update_gui_text

--- DataCreator_v001.py
+++ DataCreator_v001.py
@@ -1,6 +1,6 @@
 '''
-TODO 
 
+TODO
 
 The data portion:
 General:
@@ -10,55 +10,40 @@ UI:
 - Could make it non basic, but cba
 - Hook up variables with entry boxes and add conditional etc. For example, you should only be able to add a second variable after
 - Would be nice to have the axis variable names actually along the axis oriented as a normal graph would. NOt sure how hard/easy this would be to program.
+- Add a box for instructions.
 - Add option to enable/disable secondary variable. 
 
 Bugs:
 - Throws an error when I reset the canvas, as you can't take a mean of a slice, but it doesn't effect the functionality.
 '''
 
-import pandas as pd
+
 import tkinter as tk
 from tkinter import *
 import tkinter.colorchooser
+
+import pandas as pd
 import numpy as np
-import random, time
-from itertools import count
 import random
+from itertools import count
 from scipy.stats import pearsonr
 
-# GLOBAL VARS
-'''
-global root
-global canvas
-global clicks
-global point_ids
-global current_colour
-global animation_status
-global x_mean,x_mean_label, x_std, x_std_label, xe, e_cat
-global y_mean, y_mean_label, y_std, y_std_label, ye
-global n, n_label
-global Pearsons_R, Pearsons_R_label
-global text_box
-
-
-clicks = []   # stores the clicks that the user makes on the canvas.
-current_colour = '#000000' # Default black
-'''
 
 # Class to create and store data points
 class Particle:
+
     # this is used two assign different ids each time a class is called. There is probably  better way of doing this but it works.
     p_id = count(0)
 
-    def __init__(self, x, y, velocity):
+    def __init__(self, x, y):
         RADIUS_MODIFIER = 5
+        # increment ids by one
         self.p_id = next(self.p_id)
+
         # x and y should also be unique at this point
         self.x = x
         self.y = y
         self.category = current_colour
-        # need to set this so it has a random direction to start somehow
-        self.velocity = velocity#[1,1]
         self.point = canvas.create_oval(x-RADIUS_MODIFIER, y-RADIUS_MODIFIER, x+RADIUS_MODIFIER, y+RADIUS_MODIFIER, width = 0, fill = current_colour, tags = 'points')
         self.location = (0,0,0,0)
 
@@ -69,26 +54,8 @@ def reset_points():
     canvas.delete('points')
     clicks.clear()
     point_ids.clear()
-    global x_mean, x_mean_label, x_std, x_std_label
-    global y_mean, y_mean_label, y_std, y_std_label
-    global n, n_label
-    global Pearson_R, Pearsons_R_label
-
-    x_mean.set(getx_mean()) 
-    x_mean_label.config(text = f'X mean: {x_mean.get()}')
-    x_std.set(getx_std())
-    x_std_label.config(text = f'X std: {x_std.get()}')
-    y_mean.set(gety_mean())
-    y_mean_label.config(text = f'Y mean: {y_mean.get()}')
-    y_std.set(gety_std())
-    y_std_label.config(text = f'Y std: {y_std.get()}')
-    Pearsons_R.set(get_r())
-    Pearsons_R_label.config(text = f'Pearson\'s R: {Pearsons_R.get()}')
-    n.set(get_n())
-    n_label.config(text = f'N: {n.get()}')
-
-
-
+    #update gui
+    update_gui_text()
 
 # Recors click events and updates the screen
 def click(event):
@@ -96,37 +63,24 @@ def click(event):
     x, y = event.x, event.y
     clicks.append((x, canvas.winfo_height() - y)) # canvas.winfo_height() - y makes it so the resulting data matches the canvas
     # create a new particle
-    point_ids.append(Particle(x = x, y = y, velocity = [random.uniform(-4,4),random.uniform(-4,4)])) #
-    global x_mean, x_mean_label, x_std, x_std_label
-    global y_mean, y_mean_label, y_std, y_std_label
-    global Pearsons_R, Pearsons_R_label
-    global n, n_label 
-    x_mean.set(getx_mean()) 
-    x_mean_label.config(text = f'X mean: {x_mean.get()}')
-    x_std.set(getx_std())
-    x_std_label.config(text = f'X std: {x_std.get()}')
-    y_mean.set(gety_mean())
-    y_mean_label.config(text = f'Y mean: {y_mean.get()}')
-    y_std.set(gety_std())
-    y_std_label.config(text = f'Y std: {y_std.get()}')
-    Pearsons_R.set(get_r())
-    Pearsons_R_label.config(text = f'Pearson\'s R: {Pearsons_R.get()}') 
-    n.set(get_n())
-    n_label.config(text = f'N: {n.get()}')
-
+    point_ids.append(Particle(x = x, y = y))
+    # update the gui
+    update_gui_text()
 
 # Save the current data. This needs a complete rework to take into account the sub-variables.
 def file_save():
     #save data to a csv file
-    global xe, ye, e_cat
+    global xe, ye, e_cat, x_var_entry, y_var_entry
     data = pd.DataFrame()
     x = x_var_entry.get()
     y = y_var_entry.get()
     category = x2_var_entry.get()
-    data[x] = np.asarray(clicks)[:,0] / canvas.winfo_width()
-    data[y] = np.asarray(clicks)[:,1] / canvas.winfo_height()
+    data[x] = np.asarray(clicks)[:,0] / canvas.winfo_width()  # This changes the data so that (0,0) is SW, so it works correctly when plotting
+    data[y] = np.asarray(clicks)[:,1] / canvas.winfo_height() # This changes the data so that (0,0) is SW, so it works correctly when plotting
     data[category] = [point.category for point in point_ids]
     data.to_csv('data.csv', index = False)
+    
+    # New window that confirms file has been saved
     window = tk.Toplevel(root)
     window.title('File saved')
     T = tk.Text(window, height=1, width=30)
@@ -140,7 +94,7 @@ def choose_color():
     # variable to store hexadecimal code of color, it also retruns rgb, so I need to strip the hex code. "{rgb} #hexode" 
     color_code = tkinter.colorchooser.askcolor(title ="Choose color") 
     global current_colour 
-    current_colour= color_code[1] 
+    current_colour = color_code[1] 
     # set the colour of the second variable
     global x2_var_entry
     x2_var_entry.config(fg = current_colour)
@@ -176,7 +130,6 @@ def get_n():
 def create_grid(event=None):
     w = canvas.winfo_width() # Get current width of canvas
     h = canvas.winfo_height() # Get current height of canvas
-    #canvas.delete('grid_line') # Will only remove the grid_line
 
     # Creates all vertical lines at intevals of 100
     for i in range(0, w, 100):
@@ -186,30 +139,53 @@ def create_grid(event=None):
     for i in range(0, h, 100):
         canvas.create_line([(0, i), (w, i)], tag='grid_line')
 
+def update_gui_text():
 
+    global x_mean, x_mean_label, x_std, x_std_label
+    global y_mean, y_mean_label, y_std, y_std_label
+    global n, n_label
+    global Pearson_R, Pearsons_R_label
+
+    # This is all to change the text that appears in the gui
+    x_mean.set(getx_mean()) 
+    x_mean_label.config(text = f'X mean: {x_mean.get()}')
+    x_std.set(getx_std())
+    x_std_label.config(text = f'X std: {x_std.get()}')
+    y_mean.set(gety_mean())
+    y_mean_label.config(text = f'Y mean: {y_mean.get()}')
+    y_std.set(gety_std())
+    y_std_label.config(text = f'Y std: {y_std.get()}')
+    Pearsons_R.set(get_r())
+    Pearsons_R_label.config(text = f'Pearson\'s R: {Pearsons_R.get()}')
+    n.set(get_n())
+    n_label.config(text = f'N: {n.get()}')
+
+
+# GUI etc
 def dataCreator():
+    # GLOBAL VARS
     global root
     global canvas
     global clicks
     global point_ids
     global current_colour
     global animation_status
-    global x_mean,x_mean_label, x_std, x_std_label, xe, e_cat
-    global y_mean, y_mean_label, y_std, y_std_label, ye
+    global x_mean,x_mean_label, x_std, x_std_label, xe, e_cat, x_var_entry
+    global y_mean, y_mean_label, y_std, y_std_label, ye, y_var_entry
     global n, n_label
     global Pearsons_R, Pearsons_R_label
     global text_box
 
-
-    clicks = []   # stores the clicks that the user makes on the canvas.
+    
+    point_ids = [] # store all the particle objects.
+    clicks = []    # stores the clicks that the user makes on the canvas.
     current_colour = '#000000' # Default black
-
 
     # Create the window and draw the canvas
     # create window
     root = tk.Tk()
-    root.geometry("1300x710")
-    root.title('Draw your data')
+    root.geometry("1300x710") # arbitrary size
+    root.title('DataCreator')
 
     # Create the canvas and make it visible with pack()
     canvas = tk.Canvas(root, highlightthickness =2, highlightbackground = 'black')
@@ -220,8 +196,8 @@ def dataCreator():
 
     frame = Frame(root)
     frame.grid(row=0,column=1, sticky="n")
-    # store all the particle objects.
-    point_ids = []
+    
+    
 
 
 
@@ -285,13 +261,10 @@ def dataCreator():
     x2_var_entry = Entry(frame, textvariable=e_cat)
     x2_var_entry.grid(row=7, column =2)
     x2_var_entry.config(fg = current_colour) # This should change to the new colour
-    # Forget why I called this e_cat, but once I think of that better name
+    # Forget why I called this e_cat (maybe extra category), but once I think of that better name 
     e_cat.set('3rd Variable')
 
     text_box = Label(frame, text = 'To create another variable, change the colour.  \n All points will be sub classified by colour')
-
-
-
     frame.grid_rowconfigure(8, weight=1, minsize=100)
     text_box.grid(row = 20, column = 0, columnspan = 2)
 
@@ -301,5 +274,5 @@ def dataCreator():
     root.mainloop()
 
 
-
+#
 dataCreator()
