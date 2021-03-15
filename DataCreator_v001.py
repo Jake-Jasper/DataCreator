da389b92afb7f87840699083a648588b49073832
socalNerdtastic <socal.nerdtastic@gmail.com>
2021-03-15 10:47:55 -0700

    globalectomy and massive cleanup

--- DataCreator_v001.py
+++ DataCreator_v001.py
@@ -1,242 +1,215 @@
+#!/usr/bin/env python3
+
 import tkinter as tk
 from tkinter import Canvas, Menu, Frame, DoubleVar, StringVar, IntVar, Label, Entry
 import tkinter.colorchooser
+# ~ from itertools import count
+
 import pandas as pd
 import numpy as np
-from itertools import count
 from scipy.stats import pearsonr
-
 
 # Class to create and store data points
 class Particle:
-
     # this is used two assign different ids each time a class is called. There is probably better way of doing this but it works.
-    p_id = count(0)
+    # ~ p_id = count(0) # socal: This is as good a way as any ... but why do you need a point id?
+    current_colour = '#000000' # Default black
 
-    def __init__(self, x, y):
+    def __init__(self, canvas, x, y):
         RADIUS_MODIFIER = 5
         # increment ids by one
-        self.p_id = next(self.p_id)
+        # ~ self.p_id = next(self.p_id)
 
         # x and y should also be unique at this point
         self.x = x
         self.y = y
-        self.category = current_colour
-        self.point = canvas.create_oval(x-RADIUS_MODIFIER, y-RADIUS_MODIFIER, x+RADIUS_MODIFIER, y+RADIUS_MODIFIER, width = 0, fill = current_colour, tags = 'points')
-        self.location = (0,0,0,0)
+        self.category = self.current_colour
+        self.point = canvas.create_oval(
+            x-RADIUS_MODIFIER, y-RADIUS_MODIFIER, x+RADIUS_MODIFIER, y+RADIUS_MODIFIER,
+            width = 0, fill = self.current_colour, tags = 'points')
+        # ~ self.location = (0,0,0,0) # wth is this?
 
+class Drawing(tk.Canvas):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.point_ids = []
 
-# Function clear all data 
-def reset_points():
-    #reset data
-    canvas.delete('points')
-    clicks.clear()
-    point_ids.clear()
-    #update gui
-    update_gui_text()
+        # Create the canvas and make it visible with pack()
+        self.config(highlightthickness =2, highlightbackground = 'black')
+        # ~ canvas = tk.Canvas(root, highlightthickness =2, highlightbackground = 'black')
+        # ~ canvas.grid(row=0, column =0)
+        self.config(background = 'white', width = 800, height = 700)
 
-# Recors click events and updates the screen
-def click(event):
-# record clicks
-    x, y = event.x, event.y
-    clicks.append((x, canvas.winfo_height() - y)) # canvas.winfo_height() - y makes it so the resulting data matches the canvas
-    # create a new particle
-    point_ids.append(Particle(x = x, y = y))
-    # update the gui
-    update_gui_text()
+        # record clicks on canvas only.
+        self.bind('<Button-1>', self.click)
 
-# Save the current data. This needs a complete rework to take into account the sub-variables.
-def file_save():
-    #save data to a csv file
-    global xe, ye, e_cat, x_var_entry, y_var_entry
-    data = pd.DataFrame()
-    x = x_var_entry.get()
-    y = y_var_entry.get()
-    category = x2_var_entry.get()
-    data[x] = np.asarray(clicks)[:,0] / canvas.winfo_width()  # This changes the data so that (0,0) is SW, so it works correctly when plotting
-    data[y] = np.asarray(clicks)[:,1] / canvas.winfo_height() # This changes the data so that (0,0) is SW, so it works correctly when plotting
-    data[category] = [point.category for point in point_ids]
-    data.to_csv('data.csv', index = False)
-    
-    # New window that confirms file has been saved
-    window = tk.Toplevel(root)
-    window.title('File saved')
-    T = tk.Text(window, height=1, width=30)
-    T.pack()
-    T.insert(tk.END, 'File saved')
-    window.after(1000, lambda: window.destroy())
+        self.create_grid()
 
+    def create_grid(self):
+        """This makes the gridlines on the canvas.
+        Extend well beyond any normal monitor to pixel 3000, to avoid
+        the drama of redrawing as window sizes change"""
+        w = h = 3000
+        # Creates all vertical lines at intevals of 100
+        for i in range(0, h, 100):
+            self.create_line([(i, 0), (i, h)], tag='grid_line')
 
-# Change the color of the particles, and also will be used to identify the category a variable belongs to
-def choose_color(): 
-    # variable to store hexadecimal code of color, it also retruns rgb, so I need to strip the hex code. "{rgb} #hexode" 
-    color_code = tkinter.colorchooser.askcolor(title ="Choose color") 
-    global current_colour 
-    current_colour = color_code[1] 
-    # set the colour of the second variable
-    global x2_var_entry
-    x2_var_entry.config(fg = current_colour)
-    return color_code
+        # Creates all horizontal lines at intevals of 100
+        for i in range(0, w, 100):
+            self.create_line([(0, i), (w, i)], tag='grid_line')
 
-# get the mean in the x plane
-def getx_mean():
-    xmean = [point.x for point in point_ids]
-    return round(np.mean(xmean) / canvas.winfo_width(), 2)
+    # Function clear all data
+    def reset_points(self):
+        #reset data
+        self.delete('points')
+        self.point_ids.clear()
+        #update gui
+        self.master.update_gui_text(self.point_ids)
 
-def getx_std():
-    xstd = [point.x for point in point_ids]
-    return round(np.std(xstd) / canvas.winfo_width(), 2)
+    # Recors click events and updates the screen
+    def click(self, event):
+        # record clicks
+        # create a new particle
+        self.point_ids.append(Particle(self, event.x, event.y))
+        # update the gui
+        self.master.update_gui_text(self.point_ids)
 
-# get the mean in the y plane, need to take into account the fact that 0,0 is nw
-def gety_mean():
-    ymean = [canvas.winfo_height() - point.y for point in point_ids]
-    return round(np.mean(ymean) / canvas.winfo_height(), 2)
+#this order must match the output of the logic code
+STATS_CATS = ("X mean", "X std", "Y mean", "Y std", "Pearson's R", "N")
+class StatsFrame(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
 
-def gety_std():
-    ystd = [point.y for point in point_ids]  # don't need to normalise for canvas in this case
-    return round(np.std(ystd) / canvas.winfo_height(), 2)
+        self.data = []
+        for row_num, name in enumerate(STATS_CATS):
+            desc = tk.Label(self, text=f"{name}: ")
+            desc.grid(row=row_num, column=0, sticky='e')
+            result = tk.Label(self, text="0.0")
+            result.grid(row=row_num, column=1, sticky='w')
+            self.data.append(result)
 
-def get_r():
-    xs, ys = [point.x for point in point_ids], [point.y for point in point_ids]
-    r, p = pearsonr(xs, ys)
-    return round(-r,2)
+    def update_values(self, new_data):
+        for result_lbl, value in zip(self.data, new_data):
+            result_lbl.config(text=f"{value:.2f}")
 
-def get_n():
-    return len(point_ids)
+class VariableFrame(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
 
-# This makes the gridlines on the canvas.
-def create_grid(event=None):
-    w = canvas.winfo_width() # Get current width of canvas
-    h = canvas.winfo_height() # Get current height of canvas
+        # Add some boxes to show the stats.
+        self.xe = StringVar()
+        Label(self, text = 'X name:').grid(row=0, column=0, padx = 10)
+        self.x_var_entry = Entry(self, textvariable=self.xe)
+        self.x_var_entry.grid(row=0, column =1)
+        self.x_var_entry.config(fg = Particle.current_colour) # This should always stay the base colour
+        self.xe.set('X') # Default x label
 
-    # Creates all vertical lines at intevals of 100
-    for i in range(0, w, 100):
-        canvas.create_line([(i, 0), (i, h)], tag='grid_line')
+        self.ye = StringVar()
+        Label(self, text = 'Y name:').grid(row=1, column =0, padx =20)
+        self.y_var_entry = Entry(self, textvariable = self.ye)
+        self.y_var_entry.grid(row=1, column = 1)
+        self.ye.set('Y')
 
-    # Creates all horizontal lines at intevals of 100
-    for i in range(0, h, 100):
-        canvas.create_line([(0, i), (w, i)], tag='grid_line')
-
-def update_gui_text():
-
-    global x_mean, x_mean_label, x_std, x_std_label
-    global y_mean, y_mean_label, y_std, y_std_label
-    global n, n_label
-    global Pearson_R, Pearsons_R_label
-
-    # This is all to change the text that appears in the gui
-    x_mean.set(getx_mean()) 
-    x_mean_label.config(text = f'X mean: {x_mean.get()}')
-    x_std.set(getx_std())
-    x_std_label.config(text = f'X std: {x_std.get()}')
-    y_mean.set(gety_mean())
-    y_mean_label.config(text = f'Y mean: {y_mean.get()}')
-    y_std.set(gety_std())
-    y_std_label.config(text = f'Y std: {y_std.get()}')
-    Pearsons_R.set(get_r())
-    Pearsons_R_label.config(text = f'Pearson\'s R: {Pearsons_R.get()}')
-    n.set(get_n())
-    n_label.config(text = f'N: {n.get()}')
-
+        ### Secondary X variable -- need a better name than x2 for this.
+        self.e_cat = StringVar()
+        Label(self, text = '3rd Variable name:').grid(row=2, column=0, padx = 10)
+        self.x2_var_entry = Entry(self, textvariable=self.e_cat)
+        self.x2_var_entry.grid(row=2, column =1)
+        self.x2_var_entry.config(fg = Particle.current_colour) # This should change to the new colour
+        # Forget why I called this e_cat (maybe extra category), but once I think of that better name
+        self.e_cat.set('3rd Variable')
 
 # GUI etc
-def dataCreator():
-    # GLOBAL VARS
-    global root , canvas, clicks, point_ids, current_colour, text_box
-    global x_mean,x_mean_label, x_std, x_std_label, xe, e_cat, x_var_entry
-    global y_mean, y_mean_label, y_std, y_std_label, ye, y_var_entry
-    global n, n_label
-    global Pearsons_R, Pearsons_R_label
+class DataCreator(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master, **kwargs)
 
-    
-    point_ids = [] # store all the particle objects.
-    clicks = []    # stores the clicks that the user makes on the canvas.
-    current_colour = '#000000' # Default black
+        self.draw_window = Drawing(self)
+        self.draw_window.grid(row=0,column=0, rowspan=3)
 
-    # Create the window and draw the canvas
+        self.stats = StatsFrame(self)
+        self.stats.grid(row=0,column=1, sticky="n")
+
+        self.variables = VariableFrame(self)
+        self.variables.grid(row=0,column=2, sticky="n")
+
+        text_box = Label(self, text = 'To create another variable, change the colour.  \n All points will be sub classified by colour')
+        text_box.grid(row = 1, column = 1, columnspan = 2, sticky='n')
+
+        self.rowconfigure(2, weight=1)
+        # menu items
+        menubar = tk.Menu(self.master)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Reset", command=self.draw_window.reset_points)
+        filemenu.add_command(label="Exit", command=self.quit)
+        filemenu.add_command(label="Save", command=self.file_save)
+        menubar.add_cascade(label="Options", menu=filemenu)
+        menubar.add_command(label="Change color", command=self.choose_color) #This changes the global color
+
+        # add menubar
+        self.master.config(menu=menubar)
+
+    # Change the color of the particles, and also will be used to identify the category a variable belongs to
+    def choose_color(self):
+        # variable to store hexadecimal code of color, it also retruns rgb, so I need to strip the hex code. "{rgb} #hexode"
+        color_code = tkinter.colorchooser.askcolor(title ="Choose color")
+        current_colour = color_code[1]
+        # set the colour of the second variable
+        self.variables.x2_var_entry.config(fg = current_colour)
+
+        # set the colour of the drawing points
+        Particle.current_colour = current_colour
+
+    # Save the current data. This needs a complete rework to take into account the sub-variables.
+    def file_save(self):
+        #save data to a csv file
+        height = self.draw_window.winfo_height()
+        width = self.draw_window.winfo_width()
+        clicks = [(point.x, height-point.y) for point in self.draw_window.point_ids]
+
+        data = pd.DataFrame()
+        x = self.variables.x_var_entry.get()
+        y = self.variables.y_var_entry.get()
+        category = self.variables.x2_var_entry.get()
+        data[x] = np.asarray(clicks)[:,0] / width  # This changes the data so that (0,0) is SW, so it works correctly when plotting
+        data[y] = np.asarray(clicks)[:,1] / height # This changes the data so that (0,0) is SW, so it works correctly when plotting
+        data[category] = [point.category for point in self.draw_window.point_ids]
+        data.to_csv('data.csv', index = False)
+
+        # New window that confirms file has been saved
+        window = tk.Toplevel()
+        window.title('File saved')
+        T = tk.Text(window, height=1, width=30)
+        T.pack()
+        T.insert(tk.END, 'File saved')
+        window.after(1000, window.destroy)
+
+    def update_gui_text(self, point_ids):
+        #save data to a csv file
+        height = self.draw_window.winfo_height()
+        width = self.draw_window.winfo_width()
+        xs, ys = [point.x for point in point_ids], [point.y for point in point_ids]
+
+        # get the mean in the x plane
+        xmean = np.mean(xs) / width
+        xstd = np.std(xs) / width
+
+        # get the mean in the y plane, need to take into account the fact that 0,0 is nw
+        ymean = np.mean(ys) / height
+        ystd = np.std(ys) / height
+
+        r, p = pearsonr(xs, ys)
+        n = len(point_ids)
+        self.stats.update_values([xmean, xstd, ymean, ystd, -r, n])
+
+def main():
     # create window
     root = tk.Tk()
     root.geometry("1300x710") # arbitrary size
     root.title('DataCreator')
-
-    # Create the canvas and make it visible with pack()
-    canvas = tk.Canvas(root, highlightthickness =2, highlightbackground = 'black')
-    canvas.grid(row=0, column =0)
-    canvas.config(background = 'white', width = 800, height = 700)
-    canvas.bind('<Configure>', create_grid) # This adds the grid line every 100 piexels
-    canvas.update()
-
-    frame = Frame(root)
-    frame.grid(row=0,column=1, sticky="n")
-
-    # menu items
-    menubar = tk.Menu(root)
-    filemenu = tk.Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Reset", command=reset_points)
-    filemenu.add_command(label="Exit", command=root.quit)
-    filemenu.add_command(label="Save", command=file_save)
-    menubar.add_cascade(label="Options", menu=filemenu)
-    menubar.add_command(label="Change color", command=choose_color) #This changes the global color
-
-    # record clicks on canvas only.
-    canvas.bind('<Button-1>', click)
-
-    # add menubar
-    root.config(menu=menubar)
-
-    # Add some boxes to show the stats.
-    x_mean = DoubleVar() 
-    xe = StringVar()
-    x_mean_label = Label(frame, text = f'X mean: {x_mean.get()}')
-    x_mean_label.grid(row=0,column=0, sticky="e") # starting label
-    Label(frame, text = 'X name:').grid(row=0, column=1, padx = 10)
-    x_var_entry = Entry(frame, textvariable=xe)
-    x_var_entry.grid(row=0, column =2)
-    x_var_entry.config(fg = current_colour) # This should always stay the base colour
-    xe.set('X') # Default x label
-
-    x_std = DoubleVar()
-    x_std_label = Label(frame, text = f'X std: {x_std.get()}')
-    x_std_label.grid(row=1,column=0, sticky="e")
-
-    y_mean = DoubleVar()
-    ye = StringVar()
-    y_mean_label = Label(frame, text = f'Y mean: {y_mean.get()}')
-    y_mean_label.grid(row=2,column=0, sticky="e")
-    Label(frame, text = 'Y name:').grid(row=2, column =1, padx =20)
-    y_var_entry = Entry(frame, textvariable = ye)
-    y_var_entry.grid(row=2, column = 2)
-    ye.set('Y')
-
-    y_std = DoubleVar()
-    y_std_label = Label(frame, text = f'Y std: {y_std.get()}')
-    y_std_label.grid(row=3,column=0, sticky="e")
-
-    Pearsons_R = DoubleVar()
-    Pearsons_R_label = Label(frame, text = f'Pearson\'s R: {Pearsons_R.get()}')
-    Pearsons_R_label.grid(row=4, column = 0, sticky = 'e')
-
-    n = IntVar()
-    n_label = Label(frame, text = f'N: {n.get()}')
-    n_label.grid(row=5,column=0, sticky="e")
-
-    ### Secondary X variable -- need a better name than x2 for this.
-    global x2_var_entry
-    e_cat = StringVar()
-    Label(frame, text = '3rd Variable name:').grid(row=7, column=1, padx = 10)
-    x2_var_entry = Entry(frame, textvariable=e_cat)
-    x2_var_entry.grid(row=7, column =2)
-    x2_var_entry.config(fg = current_colour) # This should change to the new colour
-    # Forget why I called this e_cat (maybe extra category), but once I think of that better name 
-    e_cat.set('3rd Variable')
-
-    text_box = Label(frame, text = 'To create another variable, change the colour.  \n All points will be sub classified by colour')
-    frame.grid_rowconfigure(8, weight=1, minsize=100)
-    text_box.grid(row = 20, column = 0, columnspan = 2)
-
-    # run mainloop
+    win = DataCreator(root)
+    win.pack()
     root.mainloop()
 
-
-
-dataCreator()
+if __name__ == "__main__":
+    main()
